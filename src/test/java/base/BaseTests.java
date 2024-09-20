665ea9f921443bef805e43ed63dc22a2476e4869
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2024-09-20 11:26:20 +0300

    Session 4.2

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
 import reader.ReadDataFromJson;
@@ -61,10 +62,23 @@ public class BaseTests {
         ScreenRecorderUtil.startRecord(method.getName());
     }
     @AfterMethod
-    public void afterMethod(Method method) throws Exception {
+    public void afterMethod(Method method, ITestResult result) throws Exception {
         utilsTests = new UtilsTests(driver);
         utilsTests.takeScreenShot(method);
         ScreenRecorderUtil.stopRecord();
+        utilsTests.setStatus(method,result);
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        utilsTests = new UtilsTests(driver);
+        utilsTests.createReport();
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        utilsTests = new UtilsTests(driver);
+        utilsTests.flushReport();
     }
 
     public DataModel dataModel() throws FileNotFoundException {
